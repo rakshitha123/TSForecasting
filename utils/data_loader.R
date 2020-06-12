cdc1930ef2827ad27e1ea8da1f7c6273a93bfd72
rakshithawg <rakshithagw@gmail.com>
2020-06-13 00:33:09 +1000

    Modified data loader

--- utils/data_loader.R
+++ utils/data_loader.R
@@ -2,7 +2,6 @@ library(tsibble)
 library(hashmap)
 
 BASE_DIR <- "TSForecasting/ts_data/"
-TIMESTAMP_NAME <- "start_timestamp"
 
 #Extend these frequency lists as required
 LOW_FREQUENCIES <- c("10_minutes", "half_hourly", "hourly")
@@ -22,9 +21,8 @@ frequency <- NULL
 #file - .ts file path
 #value_column_name - Any name that is preferred to have as the name of the column containing series values in the returning tsibble
 #key - The name of the attribute that should be used as the key when creating the tsibble. If doesn't provide, a data frame will be returned
-#index - The name of the time attribute that should be used as the key when creating the tsibble. If doesn't provide, a data frame will be returned
-#time_attribute_name - The name of the time attribute. This can be same as the index parameter if the final goal is creating a tsibble
-convert_ts_to_tsibble <-   function(file, value_column_name = "value", key = NULL, index = NULL, time_attribute_name = TIMESTAMP_NAME){
+#index - The name of the time attribute that should be used as the index when creating the tsibble. If doesn't provide, a data frame will be returned
+convert_ts_to_tsibble <-   function(file, value_column_name = "value", key = NULL, index = NULL){
     if(is.character(file)) {
       file <- file(file, "r")
       on.exit(close(file))
@@ -35,26 +33,32 @@ convert_ts_to_tsibble <-   function(file, value_column_name = "value", key = NUL
       open(file, "r")
       on.exit(close(file))
     }
-    
+
     # Read meta-data
     col_names <- NULL
+    col_types <- NULL
 
     line <- readLines(file, n = 1) #n is no: of lines to read
-    
+
     while(length(line) && regexpr('^[[:space:]]*@data', line, perl = TRUE) == -1) { #Until read @data, run this loop (-1 indicate no match with the regular expression yet)
-      
-      if(regexpr('^[[:space:]]*@', line, perl = TRUE) > 0) { #If condition will be true for lines starting with @
-        
+
+      if(regexpr('^[[:space:]]*@', line, perl = TRUE) > 0) { #This condition will be true for lines starting with @
+
         con <- textConnection(line)
-        line <- scan(con, character(), quiet = TRUE) #Creating a vector containing the words in line (ex: "@attribute" "series_name")
+        line <- scan(con, character(), quiet = TRUE) #Creating a vector containing the words in a line (ex: "@attribute" "series_name" "string")
         close(con)
-        
-        if(length(line) != 2)
-          stop("Invalid meta-data specification.")
-        else{
-          if(line[1] == "@attribute")
-            col_names <- c(col_names, line[2])
-          else if(line[1] == "@frequency")
+
+        if(line[1] == "@attribute"){
+          if(length(line) != 3)  #Attributes have both name and type
+            stop("Invalid meta-data specification.")
+
+          col_names <- c(col_names, line[2])
+          col_types <- c(col_types, line[3])
+        }else{
+          if(length(line) != 2) #Other meta-data have only values
+            stop("Invalid meta-data specification.")
+
+          if(line[1] == "@frequency")
             frequency <- line[2]
           else if(line[1] == "@horizon")
             forecast_horizon <- as.numeric(line[2])
@@ -62,42 +66,46 @@ convert_ts_to_tsibble <-   function(file, value_column_name = "value", key = NUL
       }
       line <- readLines(file, n = 1)
     }
-    
+
     if(length(line) == 0)
       stop("Missing data section.")
     if(is.null(col_names))
       stop("Missing attribute section.")
-    
+
     line <- readLines(file, n = 1)
-    
+
     if(length(line) == 0)
       stop("Missing series information under data section.")
-    
+
     for(col in col_names)
       assign(col, NULL)
-    
+
     values <- NULL
     row_count <- 0
-    
+
     # Get data
     while(length(line) != 0){
       full_info <- strsplit(line, ":")[[1]]
-      
+
       if(length(full_info) != length(col_names)+1)
         stop("Missing attributes/values in series.")
-      
+
       series <- as.numeric(strsplit(tail(full_info, 1), ",")[[1]])
-      
+
       if(sum(is.na(series)) == length(series))
         stop("All series values are missing. A given series should contains a set of comma separated numeric values. At least one numeric value should be there in a series.")
-      
+
       values <- c(values, series)
       row_count <- row_count + length(series)
-      
+
       attributes <- head(full_info, length(full_info)-1)
-      
+
       for(col in 1:length(col_names)){
-        if(col_names[col] == time_attribute_name){
+
+        att <- eval(parse(text=col_names[col]))
+
+        #This format supports 3 attribute types: string, numeric and date
+        if(col_types[col] == "date"){
           if(is.null(frequency))
             stop("Frequency is missing.")
           else{
@@ -107,37 +115,46 @@ convert_ts_to_tsibble <-   function(file, value_column_name = "value", key = NUL
                 start_time <- as.Date(attributes[col], format = "%Y-%m-%d %H-%M-%S")
             else
                 stop("Invalid frequency.")
-            
+
             if(is.na(start_time))
               stop("Incorrect timestamp format. Specify your timestamps as YYYY-mm-dd HH-MM-SS")
           }
-          
-          att <- seq(start_time, length=length(series), by=FREQ_MAP[[frequency]])
+
+          att <- append(att, seq(start_time, length=length(series), by=FREQ_MAP[[frequency]]))
         }else{
-          att <- eval(parse(text=col_names[col]))
-          att <- c(att, rep(attributes[col], length(series)))
+          if(col_types[col] == "numeric"){
+            attributes[col] <- as.numeric(attributes[col])
+          }else if(col_types[col] == "string"){
+            attributes[col] <- as.character(attributes[col])
+          }else
+            stop("Invalid attribute type.")
+
+          if(is.na(attributes[col]))
+            stop("Invalid attribute values.")
+
+          att <- append(att, rep(attributes[col], length(series)))
         }
         assign(col_names[col], att)
       }
-      
+
       line <- readLines(file, n = 1)
     }
-    
+
     data <- as.data.frame(matrix(nrow = row_count, ncol = length(col_names) + 1))
     colnames(data) <- c(col_names, value_column_name)
-    
+
     for(col in col_names)
       data[[col]] <- eval(parse(text=col))
-    
+
     data[[value_column_name]] <- values
-    
+
     if(!(is.null(key) | is.null(index))){
       if(!(key %in% col_names & index %in% col_names))
         stop("Invalid key and index. Cannot convert the dataframe into tsibble format.")
-      
+
       data <- as_tsibble(data, key = key, index = index)
     }
-    
+
     data
 }
 
